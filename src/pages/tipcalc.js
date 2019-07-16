6b2571a54b35aad040f304b1fce40f6f4b4ce31f
lancep888 <lance.pu@sema4genomics.com>
2019-07-16 13:09:47 -0400

    starting work on tip calculator

--- src/pages/tipcalc.js
+++ src/pages/tipcalc.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import SEO from '../components/seo'
-import Layout from '../components/layout'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import SEO from "../components/seo";
+import Layout from "../components/layout";
 
-const TipCalc = () => {
+class TipCalc extends Component {
+  state = { data: {} };
+  handleInputChange = ({ currentTarget: input }) => {
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+    this.setState({ data }, () => console.log(this.state.data));
+  };
+
+  calculateAmount = () => {
+    const { billAmount, taxAmount, personCount } = this.state.data;
+    const amountPerPerson =
+      (parseFloat(billAmount) + parseFloat(taxAmount)) / parseInt(personCount);
+    alert(amountPerPerson);
+  };
+
+  render() {
     return (
-        <React.Fragment>
-            <SEO />
-            <Layout>
-                <div>Tip Calculator</div>
-            </Layout>
-        </React.Fragment>
-    )
+      <React.Fragment>
+        <SEO />
+        <Layout>
+          <div>Tip Calculator</div>
+          <input
+            onChange={this.handleInputChange}
+            placeholder="Bill Amount (Before Tax)"
+            type="number"
+            name="billAmount"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="Tax Amount"
+            type="number"
+            name="taxAmount"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="How Many people"
+            name="personCount"
+            type="number"
+          />
+          <button onClick={this.calculateAmount}>Calculate</button>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
 
-export default TipCalc
+export default TipCalc;
