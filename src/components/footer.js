0b2e68774e2ed1f442387d74b0a5d24ae7e1a0a9
lancepu <contact@lancepu.dev>
2019-08-29 18:15:19 -0400

    update profilepic to webp and add resume

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
+import resume from "../resume/LancePu_Resume.pdf";
 
 const Footer = () => (
   <footer>
-        <a
-          href="https://www.linkedin.com/in/lancepu/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          linkedin
-        </a>{' '}
-        &ndash;{' '}
-        <a
-          href="https://github.com/lancepu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github
-        </a>{' '}
-        &ndash;{' '}
-        <a
-          href="mailto:contact@lancepu.dev"
-          target="_top"
-          rel="noopener noreferrer"
-        >
-          email
-        </a>
-      </footer>
-)
+    <a
+      href="https://www.linkedin.com/in/lancepu/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      linkedin
+    </a>{" "}
+    &ndash;{" "}
+    <a
+      href="https://github.com/lancepu"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      github
+    </a>{" "}
+    &ndash;{" "}
+    <a
+      href="mailto:contact@lancepu.dev"
+      target="_top"
+      rel="noopener noreferrer"
+    >
+      email
+    </a>
+    &ndash;{" "}
+    <a href={resume} target="_blank" rel="noopener noreferrer">
+      Resume
+    </a>
+  </footer>
+);
 
-export default Footer
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import SEO from '../components/seo'
-import Layout from '../components/layout'
-import profilePic from "../images/lancepu_profile.png"
+import React from "react";
+import SEO from "../components/seo";
+import Layout from "../components/layout";
+import profilePic from "../images/lancepu_profile.webp";
 
 const Index = () => {
-    return (
-        <React.Fragment>
-            <SEO />
-            <Layout>
-                <h3>Hello World</h3>
-                <a href="/about"><img className="avatar" src={profilePic} alt="Lance Pu"/></a>
-                <p style={{fontSize: "20px"}}>Welcome to my <a href="/project">portfolio</a> / <a href="/blog">blog</a> / <a href="/tool">utility belt</a></p>
-            </Layout>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <SEO />
+      <Layout>
+        <h3>Hello World</h3>
+        <a href="/about">
+          <img className="avatar" src={profilePic} alt="Lance Pu" />
+        </a>
+        <p style={{ fontSize: "20px" }}>
+          Welcome to my <a href="/project">portfolio</a> /{" "}
+          <a href="/blog">blog</a> / <a href="/tool">utility belt</a>
+        </p>
+      </Layout>
+    </React.Fragment>
+  );
+};
 
-export default Index
+export default Index;
